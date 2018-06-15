32fa5b54b6b2edd21bd9c408650a1a12fcb0e5d5
Nipuna H Herath <nipuna@omobio.net>
2018-06-15 09:38:37 +0530

    Create bridging header and Swift class bridge

---
+++ ios/CalendarModule/SwiftCalendarManager.swift
@@ -0,0 +1,18 @@
+//
+//  Swift Demo.swift
+//  TestProject
+//
+//  Created by Nipuna H Herath on 6/15/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import UIKit
+
+class Swift_Demo: NSObject {
+
+  func greet(person: String) -> String {
+    let greeting = "Hello, " + person + "!"
+    return greeting
+  }
+  
+}
---
+++ ios/CalendarModule/SwiftCalendarManagerBridge.m
@@ -0,0 +1,9 @@
+//
+//  SwiftCalendarManagerBridge.m
+//  TestProject
+//
+//  Created by Nipuna H Herath on 6/15/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
