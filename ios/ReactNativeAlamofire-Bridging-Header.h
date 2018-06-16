004927a3c5a7cc9126d5646e84a7b1788e1f554f
Nipuna H Herath <hash.crackhead@gmail.com>
2018-06-16 10:49:48 +0530

    remove project

---
+++ ios/ReactNativeAlamofire-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ReactNativeAlamofire-Bridging-Header.h
+//  ReactNativeAlamofire
+//
+//  Created by Nipuna H Herath on 6/16/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#ifndef ReactNativeAlamofire_Bridging_Header_h
+#define ReactNativeAlamofire_Bridging_Header_h
+
+
+#endif /* ReactNativeAlamofire_Bridging_Header_h */
