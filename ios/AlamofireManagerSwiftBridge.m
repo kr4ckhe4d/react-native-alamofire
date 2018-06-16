584e07710d10e2ca46336a2ffd14c871b69dbd6a
Nipuna H Herath <hash.crackhead@gmail.com>
2018-06-16 10:45:07 +0530

    Add Swift bridge, Alamofire swift manager

---
+++ ios/AlamofireManagerSwiftBridge.m
@@ -0,0 +1,9 @@
+//
+//  AlamofireManagerSwiftBridge.m
+//  ReactNativeAlamofire
+//
+//  Created by Nipuna H Herath on 6/16/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/Classes/AlamofireManagerSwift.swift
@@ -0,0 +1,9 @@
+//
+//  AlamofireManagerSwift.swift
+//  ReactNativeAlamofire
+//
+//  Created by Nipuna H Herath on 6/16/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import Foundation
